0fd5c64a6f28837bfc6d74218895467d99b3757f
martindevox2811 <manhdaokhacmanh@gmail.com>
2022-06-08 00:13:18 +0700

    feat: init paypal buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import Button from "./button";
+
+const paypalScriptOptions = {
+  "client-id":
+    "AUZOZrWWVyN7_TzvmZrj8NmXwsh1LyXiyBSJAim8BAhdNBNtwC6Y79TcVV4kKyi69_n4rmDTVD0pwTMT",
+  currency: "USD",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PayPalScriptProvider options={paypalScriptOptions}>
+          <Button />
+        </PayPalScriptProvider>
       </header>
     </div>
   );
---
+++ src/button.js
@@ -0,0 +1,49 @@
+import { usePayPalScriptReducer, PayPalButtons } from "@paypal/react-paypal-js";
+export default function Button() {
+  /**
+   * usePayPalScriptReducer use within PayPalScriptProvider
+   * isPending: not finished loading(default state)
+   * isResolved: successfully loaded
+   * isRejected: failed to load
+   */
+  const [{ isPending }] = usePayPalScriptReducer();
+  const paypalbuttonTransactionProps = {
+    style: { layout: "vertical" },
+    createOrder(data, actions) {
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: "0.01",
+            },
+          },
+        ],
+      });
+    },
+    onApprove(data, actions) {
+      /**
+       * data: {
+       *   orderID: string;
+       *   payerID: string;
+       *   paymentID: string | null;
+       *   billingToken: string | null;
+       *   facilitatorAccesstoken: string;
+       * }
+       */
+      return actions.order.capture({}).then((details) => {
+        alert(
+          "Transaction completed by" +
+            (details?.payer.name.given_name ?? "No details")
+        );
+
+        alert("Data details: " + JSON.stringify(data, null, 2));
+      });
+    },
+  };
+  return (
+    <>
+      {isPending ? <h2>Load Smart Payment Button...</h2> : null}
+      <PayPalButtons {...paypalbuttonTransactionProps} />
+    </>
+  );
+}
